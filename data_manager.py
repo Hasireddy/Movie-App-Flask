ef1ad7b9633bde8aa011fd51cfe57f79dcaf5dc6
Hasireddy <malikireddy.uma@gmail.com>
2026-02-28 10:05:00 +0100

    Basic setup

---
+++ data_manager.py
@@ -0,0 +1,7 @@
+from models import db, User, Movie
+
+
+"""The DataManager class will use SQLAlchemy to interact with SQLite database."""
+
+class dataManager():
+    pass
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#User model
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+
+
+#Movie model
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    director = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    poster_url = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
