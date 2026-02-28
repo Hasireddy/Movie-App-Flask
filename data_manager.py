98abefb5a242d3b519978fdb11129d97da4fa614
Hasireddy <malikireddy.uma@gmail.com>
2026-02-28 10:19:54 +0100

    SQLAlchemy setup

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,32 @@
 from models import db, User, Movie
 
+#The DataManager class will use SQLAlchemy to interact with SQLite database.It performs all CRUD operations
 
-"""The DataManager class will use SQLAlchemy to interact with SQLite database."""
+class DataManager():
+    def __init__(self, name):
+        self.name = name
 
-class dataManager():
-    pass
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    def get_users(self):
+        users = User.query.all()
+        return users
+
+    def get_movies(self,user_id):
+        pass
+
+    def add_movie(self, movie):
+        pass
+
+    def update_movie(self, movie_id, new_title):
+        pass
+
+    def delete_movie(self,movie_id):
+        pass
+
+
+
